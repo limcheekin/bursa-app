b32144adb204cb5c61e64bb8c875f58df689bc0f
Lim Chee Kin <limcheekin@vobject.com>
2012-03-10 23:24:32 +0800

    completed html parsing.

--- war/WEB-INF/routes.groovy
+++ war/WEB-INF/routes.groovy
@@ -1,5 +1,6 @@
 
 get "/", forward: "/WEB-INF/pages/index.gtpl"
 get "/datetime", forward: "/datetime.groovy"
+get "/htmlParsing", forward: "/htmlParsing.groovy"
 
 get "/favicon.ico", redirect: "/images/gaelyk-small-favicon.png"
